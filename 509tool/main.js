2e45e2a1b82b2ae938330eda76fc97b19ff40391
arslewgithub <127671284+arslewgithub@users.noreply.github.com>
2023-08-28 22:37:27 +0800

    Add files via upload

---
+++ 509tool/main.js
@@ -0,0 +1,22 @@
+function searchClear() {
+    if (event.keyCode == 13) {
+        setTimeout('document.getElementById("gsearch").value = ""', 10)
+    }
+}
+
+document.getElementById('gsearch').onkeydown = searchClear;
+function initialization() {
+    var xl = document.getElementsByClassName('x').length
+    for (let e = 0; e < xl; e++) {
+        document.getElementsByClassName('x')[xl - e - 1].classList.remove('x');
+    }
+    var ind = document.getElementsByTagName('img').length
+    for (let a = 0; a < ind; a++) {
+        document.getElementsByTagName('img')[a].draggable = false;
+    }
+    var and = document.getElementsByTagName('img').length
+    for (let p = 0; p < and; p++) {
+        document.getElementsByTagName('a')[p].draggable = false;
+    }
+}
+initialization()
