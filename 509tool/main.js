3210e69192804573a4af6505e870221da7915d01
arslewgithub <127671284+arslewgithub@users.noreply.github.com>
2023-08-28 23:54:13 +0800

    Add files via upload

--- 509tool/main.js
+++ 509tool/main.js
@@ -19,6 +19,13 @@ function initialization() {
     for (let p = 0; p < and; p++) {
         document.getElementsByTagName('a')[p].draggable = false;
     }
+
+    for (let w = 1; w < document.getElementsByClassName('funbt').length; w++) {
+        document.getElementsByClassName('funbt')[w].onclick = function(){
+            alert('❌此功能正在開發中，尚未開放使用！\n❓如果對於其他功能使用有問題，請洽詢開發人員！\n')
+        };
+    }
 }
 
 initialization()
+
