13a990fb7ff494c6b200795c6c3852de33ffdb88
arslewgithub <127671284+arslewgithub@users.noreply.github.com>
2023-10-25 00:29:59 +0800

    Add files via upload

--- hwshbirthday/assets/js/main.js
+++ hwshbirthday/assets/js/main.js
@@ -19,5 +19,38 @@ function dbupdate(collection, doc, updateContent) {
     return db.collection(String(collection)).doc(String(doc)).update(updateContent);
 }
 function dbSnapshot(collection, doc) {
-    return db.collection(String(collection)).doc(String(doc)).onSnapshot((dbdoc) => {dbt = dbdoc.data()});
+    return db.collection(String(collection)).doc(String(doc)).onSnapshot((dbdoc) => { dbt = dbdoc.data() });
+}
+
+function getCopyUserData() {
+    var list = {};
+    db.collection('userdata')
+        .get().then(qS => {
+            qS.forEach(async doc => {
+                var c = doc.data();
+                list[String(doc.id)] = JSON.parse(JSON.stringify(c));
+            });
+        });
+    return list;
+}
+
+async function setCantake(num) {
+    var userarr = getCopyUserData();
+    await dbget('database', 'arraybase').then(async (v) => {
+        arrbs = v.data().order;
+        await dbget('database', 'noodle').then(async (k) => {
+            var n = k.data().nowok + num;
+            carray = [];
+            for (var i = 0; i < arrbs.length; i++) {
+                if (userarr[arrbs[i]].cantake != (n - userarr[arrbs[i]].noodle > 0)) {
+                    dbupdate('userdata', arrbs[i], {
+                        cantake: (n - userarr[arrbs[i]].noodle >= 0)
+                    })
+                }
+                carray.push((n - userarr[arrbs[i]].noodle >= 0))
+                n -= userarr[arrbs[i]].noodle;
+            }
+            dbupdate('database', 'arraybase', { cantakelist: carray })
+        })
+    })
 }
